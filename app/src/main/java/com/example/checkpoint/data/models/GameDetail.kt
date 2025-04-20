00cea0b3fae0d16ddeef8557a6649344f95161a9
lusanmanso <lusanmanso@gmail.com>
2025-04-20 19:27:55 +0200

    feat(api): add GameDetail endpoint

---
+++ app/src/main/java/com/example/checkpoint/data/models/GameDetail.kt
@@ -0,0 +1,4 @@
+package com.example.checkpoint.data.models
+
+class GameDetail {
+}
--- app/src/main/java/com/example/checkpoint/data/network/ApiRAWG.kt
+++ app/src/main/java/com/example/checkpoint/data/network/ApiRAWG.kt
@@ -1,18 +1,26 @@
 package com.example.checkpoint.data.network
 
 import com.example.checkpoint.data.models.GameResponse
+import com.example.checkpoint.data.models.GameDetail
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface ApiRAWG {
     @GET("games")
     suspend fun getGames(
         @Query("key") apiKey: String,
-        @Query("page") page: Int? = 1,
-        @Query("page_size") pageSize: Int? = 20,
-        @Query("search") search: String? = null,
+        @Query("page") page: Int,
+        @Query("page_size") pageSize: Int,
+        // @Query("search") search: String? = null,
         // TODO: Add more queries for filtering
     ): Response<GameResponse>
+
+    @GET("games/{id}")
+    suspend fun getGameDetails(
+        @Path("id") gameId: Int,
+        @Query("key") apiKey: String
+    ): Response<GameDetail>
 
 }

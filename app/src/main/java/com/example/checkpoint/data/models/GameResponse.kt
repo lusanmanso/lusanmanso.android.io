5174f3714fc32a8dbcc6f09b998e6d7f59f9b577
lusanmanso <lusanmanso@gmail.com>
2025-05-01 17:24:53 +0200

    refactor(data): remove count and previous from GameResponse

--- app/src/main/java/com/example/checkpoint/data/models/GameResponse.kt
+++ app/src/main/java/com/example/checkpoint/data/models/GameResponse.kt
@@ -2,8 +2,6 @@ package com.example.checkpoint.data.models
 
 // Overall struct of the endpoint response
 class GameResponse(
-    val count: Int,
     val next: String, // Url to the next page
-    val previous: String, // Url to the previous page
     val results: List<Game>
 )
